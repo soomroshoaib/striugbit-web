8c465cfee5475544657818c89079ff9f04f98e9f
soomroshoaib <soomroshoaib178@gmail.com>
2023-05-26 06:09:28 -0700

    first commit

---
+++ src/components/common/header/Head.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container flexSB'>
+          <div className='logo'>
+            <h1>   STRUGBITS </h1>
+            <span>SOFTWARE  COMPANY </span>
+          </div>
+
+          <div className='social'>
+            <i className='fab fa-facebook-f icon'></i>
+            <i className='fab fa-instagram icon'></i>
+            <i className='fab fa-twitter icon'></i>
+            <i className='fab fa-youtube icon'></i>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
+
+
+
---
+++ src/components/home/hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import Heading from "../../common/heading/Heading"
+import "./Hero.css"
+
+const Hero = () => {
+  return (
+    <>
+      <section className='hero'>
+        <div className='container'>
+          <div className='row'>
+            <Heading subtitle='WELCOME TO STRUGBITS' title='Best Web Development Expertise' />
+            <p>Opening a new business or revamping your old one, Strugbits has all your marketing needs covered. Time to create your own creative digital outlet. Are you ready?</p>
+            <div className='button'>
+              <button className='primary-btn'>
+                GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+              <button>
+                VIEW COURSE <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className='margin'></div>
+    </>
+  )
+}
+
+export default Hero
